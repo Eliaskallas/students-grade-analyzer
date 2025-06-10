3124b717b65c6b69914a9e15a292e75b0e04fbd7
Eliaskallas <eliaskallas@live.com>
2025-06-10 14:31:02 +0300

    Write the function to print the highest grade earned in the class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 def display_student_summary (S,G):
     
     for i in range(len(S)):
-        print('Name : ', S[i] , ', Grade :', G[i])
+        print('Name :', S[i] , ', Grade :', G[i])
 
 # function 'get_avg_grade' : Prints the average grade of the class
 
@@ -16,20 +16,30 @@ def get_avg_grade (G):
 
 # function 'get_heighest_grade' : Prints the highest grade earned (Student name and grade)
 
+def get_heighest_grade (S,G):
+
+    j = 0
+    k = 0
+    for i in range(len(S)):
+        if j < float(G[i]):
+            j = float(G[i])
+            k = i
+    print('The highest grade is', j, 'earned by :', S[k])
+
 # function 'count_passed' : Prints the count of students who passed (grade >= 60)
 
 # main program
 
-num_of_students = int(input('Kindly enter the number of students :'))
+num_of_students = int(input('Kindly enter the number of students : '))
 while num_of_students <= 0:
     print('The number of the students must be positive')
-    num_of_students = int(input('Kindly enter the number of students :'))
+    num_of_students = int(input('Kindly enter the number of students : '))
 
 names = []
 grades = []
 
 while num_of_students > 0:
-    name = input('Enter the name of the student :')
+    name = input('Enter the name of the student : ')
     names.append(str(name))
     grade = float(input('Enter his grade : '))
     while grade < 0 or grade > 100:
@@ -39,4 +49,5 @@ while num_of_students > 0:
     num_of_students -= 1
 
 display_student_summary (names,grades)
-print('The average grade of the class is : ', get_avg_grade (grades))
+print('The average grade of the class is :', get_avg_grade (grades))
+get_heighest_grade (names,grades)
